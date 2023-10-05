8d2fd8a15487c20624be16ce094dd900291f637c
CiberSPEPSAviles <100194632+CiberSPEPSAviles@users.noreply.github.com>
2023-10-05 16:10:47 +0200

    Nueva versión optimizada

--- JDBCTableModel.java
+++ JDBCTableModel.java
@@ -1,117 +1,116 @@
-import javax.swing.table.*; 
-import java.sql.*; 
+import javax.swing.table.*;
+import java.sql.*;
 import java.util.*;
 /* TableModel inmutable construido 
   de los metadatos de una tabla en una BD JDBC 
-*/ 
-public class JDBCTableModel extends AbstractTableModel {
-        Object[][] contents;
-        String[] columnNames;
-        Class[] columnClasses;
-    // El constructor por defecto requiere conexión y nombre de tabla
-    public JDBCTableModel (Connection conn,
-                       String tableName)
+ */
+public final class JDBCTableModel extends AbstractTableModel {
+    Object[][] contents;
+    ArrayList<String> columnNames;
+    ArrayList<Class> columnClasses;
+    /* El constructor por defecto requiere conexión y nombre de tabla */
+    public JDBCTableModel(Connection conn,
+            String tableName)
             throws SQLException {
-            super();
-            getTableContents (conn, tableName);
+        super();
+        getTableContents(conn, tableName);
     }
-    // Método de obtención de contenidos de tabla
-    protected void getTableContents (Connection conn,
-                                 String tableName)
-                throws SQLException {
-        // getMetaData: qué columnas existen y
-        // de qué tipo (clase) son
+    /* Método de obtención de contenidos de tabla */
+    protected void getTableContents(Connection conn,
+            String tableName)
+            throws SQLException {
+        /* getMetaData: qué columnas existen y
+         de qué tipo (clase) son */
         DatabaseMetaData meta = conn.getMetaData();
-        System.out.println ("Metadatos obtenidos = " + meta);
-        ResultSet results =
-                meta.getColumns (null, null, tableName, null) ;
-        System.out.println ("Obtenidos resultados de columna");
-        ArrayList colNamesList = new ArrayList();
-        ArrayList colClassesList = new ArrayList();
+        System.out.println("Metadatos obtenidos = " + meta);
+        ResultSet results
+                = meta.getColumns(null, null, tableName, null);
+        System.out.println("Obtenidos resultados de columna");
+        columnNames = new ArrayList<>();
+        columnClasses = new ArrayList<>();
         while (results.next()) {
-            colNamesList.add (results.getString ("COLUMN_NAME")); 
-            System.out.println ("Nombre: " + 
-                    results.getString ("COLUMN_NAME"));
-            int dbType = results.getInt ("DATA_TYPE");
+            columnNames.add(results.getString("COLUMN_NAME"));
+            System.out.println("Nombre: "
+                    + results.getString("COLUMN_NAME"));
+            int dbType = results.getInt("DATA_TYPE");
+            /* Nuevo formato de switch desde JDK 13 */
             switch (dbType) {
-                case Types.INTEGER:
-                    colClassesList.add (Integer.class); 
-                    break; 
-                case Types.FLOAT:
-                    colClassesList.add (Float.class); 
-                    break; 
-                case Types.DOUBLE: 
-                case Types.REAL:
-                    colClassesList.add (Double.class); break; 
-                case Types.DATE: 
-                case Types.TIME: 
-                case Types.TIMESTAMP:
-                    colClassesList.add (java.sql.Date.class); break; 
-                default:
-                    colClassesList.add (String.class); break; 
-            }; 
-            System.out.println ("Tipo: " +
-                    results.getInt ("DATA_TYPE"));
+                case Types.INTEGER ->
+                    columnClasses.add(Integer.class);
+                case Types.FLOAT ->
+                    columnClasses.add(Float.class);
+                case Types.DOUBLE, Types.REAL ->
+                    columnClasses.add(Double.class);
+                case Types.DATE, Types.TIME, Types.TIMESTAMP ->
+                    columnClasses.add(java.sql.Date.class);
+                default ->
+                    columnClasses.add(String.class);
+            }
+            System.out.println("Tipo: "
+                    + results.getInt("DATA_TYPE"));
         }
-        columnNames = new String [colNamesList.size()];
-        colNamesList.toArray (columnNames);
-        columnClasses = new Class [colClassesList.size()];
-        colClassesList.toArray (columnClasses);
-        // Obtiene todos los datos de la tabla
-        // y los coloca en un array de contenidos
-        Statement statement = conn.createStatement ();
-        results = statement.executeQuery ("SELECT * FROM " +tableName);
+        /* Obtiene todos los datos de la tabla
+         y los coloca en un ArrayList de contenidos */
+        Statement statement = conn.createStatement();
+        results = statement.executeQuery("SELECT * FROM " + tableName);
         ArrayList rowList = new ArrayList();
         while (results.next()) {
-            ArrayList cellList = new ArrayList(); 
-            for (int i = 0; i<columnClasses.length; i++) { 
+            ArrayList cellList = new ArrayList();
+            for (int i = 0; i < columnClasses.size(); i++) {
                 Object cellValue = null;
-                if (columnClasses[i] == String.class) 
-                    cellValue = results.getString (columnNames[i]); 
-                else if (columnClasses[i] == Integer.class) 
-                    cellValue = Integer.valueOf(results.getInt (columnNames[i])); 
-                else if (columnClasses[i] == Float.class) 
-                    cellValue = Float.valueOf(results.getInt (columnNames[i])); 
-                else if (columnClasses[i] == Double.class) 
-                    cellValue = Double.valueOf(results.getDouble (columnNames[i]));
-                else if (columnClasses[i] == java.sql.Date.class) 
-                    cellValue = results.getDate (columnNames[i]); 
-                else 
-                    System.out.println ("No puedo asignar " + columnNames[i]);
-                cellList.add (cellValue);
+                if (columnClasses.get(i) == String.class) {
+                    cellValue = results.getString(columnNames.get(i));
+                } else if (columnClasses.get(i) == Integer.class) {
+                    cellValue = results.getInt(columnNames.get(i));
+                } else if (columnClasses.get(i) == Float.class) {
+                    cellValue = results.getFloat(columnNames.get(i));
+                } else if (columnClasses.get(i) == Double.class) {
+                    cellValue = results.getDouble(columnNames.get(i));
+                } else if (columnClasses.get(i) == java.sql.Date.class) {
+                    cellValue = results.getDate(columnNames.get(i));
+                } else {
+                    System.out.println("No puedo asignar " + columnNames.get(i));
+                }
+                cellList.add(cellValue);
             }
             Object[] cells = cellList.toArray();
-            rowList.add (cells);
+            rowList.add(cells);
         }
-        // Finalmente se crea el array bidimensional de contenidos
-        contents = new Object[rowList.size()] [];
-        for (int i=0; i<contents.length; i++)
-            contents[i] = (Object []) rowList.get (i);
-        System.out.println ("Modelo creado con " +
-            contents.length + " filas");
+        /* Finalmente se crea el array bidimensional de contenidos */
+        contents = new Object[rowList.size()][];
+        for (int i = 0; i < contents.length; i++) {
+            contents[i] = (Object[]) rowList.get(i);
+        }
+        System.out.println("Modelo creado con "
+                + contents.length + " filas");
         results.close();
         statement.close();
     }
-    // Métodos de AbstractTableModel
+    /* Métodos de AbstractTableModel */
+    @Override
     public int getRowCount() {
         return contents.length;
     }
+    @Override
     public int getColumnCount() {
-        if (contents.length == 0)
+        if (contents.length == 0) {
             return 0;
-        else
+        } else {
             return contents[0].length;
+        }
     }
-    public Object getValueAt (int row, int column) {
-        return contents [row][column];
+    @Override
+    public Object getValueAt(int row, int column) {
+        return contents[row][column];
     }
-
-    // Métodos de sobrecarga para los cuales AbstractTableModel
-    // tiene implementaciones
-    public Class getColumnClass (int col) {
-        return columnClasses [col];
+    /* Métodos de sobrecarga para los cuales AbstractTableModel
+    tiene implementaciones */
+    @Override
+    public Class getColumnClass(int col) {
+        return columnClasses.get(col);
     }
-    public String getColumnName (int col) { 
-        return columnNames [col]; 
-    } 
+    @Override
+    public String getColumnName(int col) {
+        return columnNames.get(col);
+    }
 }
